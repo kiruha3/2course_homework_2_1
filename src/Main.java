a6b421354f4073cd1f3657ce02a63aa9f00e1f8e
kirill <kiruha3@mail.ru>
2023-07-26 20:15:08 +0700

    5 и 6 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+//Напишите метод, который изменяет значение переменной типа int.
+//        Объявите метод changeValue, который принимает параметр типа int
+//        под названием value. Внутри метода с помощью оператора присваивания
+//        запишите в параметр значение 22.
+//        В методе main объявите переменную типа int под названием value
+//        и запишите туда значение 33. В следующей строке вызовите метод
+//        changeValue и передайте в него эту переменную.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int value = 33;
+        System.out.println("Задание 5 и 6 идентичны " + changeValue(value));
     }
+    private static int changeValue(int value) {
+        return value = 33;//задание 5\6
+    }
+
 }
